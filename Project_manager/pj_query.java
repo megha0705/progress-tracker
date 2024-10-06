6cc839ab6dcd9c6099c157239d530e81c2ee64ba
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2024-10-06 19:26:35 +0530

    correcting query

--- Project_manager/pj_query.java
+++ Project_manager/pj_query.java
@@ -10,4 +10,8 @@ public class pj_query {
    String pj_name_id = "SELECT * FROM projects";
    return pj_name_id;
   }
+  public static String selectTable(){
+    String joinPjTask = "SELECT p.project_id, p.project_name, t.project_id,  t.taskName, t.taskDescription ,t.timelimit FROM task t INNER JOIN projects p ON p.project_id = t.project_id";
+    return joinPjTask;
+  }
 }
