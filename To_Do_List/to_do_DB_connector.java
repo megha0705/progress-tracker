2d3cb7352defc960df1788d49a465a42b5083338
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2024-10-13 19:04:52 +0530

    comments added

--- To_Do_List/to_do_DB_connector.java
+++ To_Do_List/to_do_DB_connector.java
@@ -2,31 +2,26 @@ package To_Do_List;
 import database.DB_Connection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
-import com.mysql.cj.xdevapi.PreparableStatement;
-
 public class to_do_DB_connector {
+    //function for inserting task on to the "task" table
     public static void insertTask(to_do_manager tdm){
         try{
             Connection conn = DB_Connection.connect();
-
-        String query = to_do_query.getInsert();
-
-        PreparedStatement Pstm =conn.prepareStatement(query);
-        
-        Pstm.setString(1,tdm.getName());
-        Pstm.setString(2,tdm.getDescription());
-        Pstm.setDate(3,tdm.timeLimit());
-        Pstm.setInt(4,tdm.getProjectId());
-        System.out.println(Pstm);
-        Pstm.execute();
-        System.out.println("data inserted succesfully");
-        }catch(Exception e){
-            e.getStackTrace();
-        }
-        
-
+            String query = to_do_query.getInsert();
+            PreparedStatement Pstm =conn.prepareStatement(query);
+            Pstm.setString(1,tdm.getName());
+            Pstm.setString(2,tdm.getDescription());
+            Pstm.setDate(3,tdm.timeLimit());
+            Pstm.setInt(4,tdm.getProjectId());
+            System.out.println(Pstm);
+            Pstm.execute();
+            System.out.println("data inserted succesfully");
+            }catch(Exception e){
+                e.getStackTrace();
+            }
     }
+
+    //allowing the user to update their status , function for that
     public static void updateStatus(to_do_manager tdm){
         try{
             Connection con = DB_Connection.connect();
--- To_Do_List/to_do_manager.java
+++ To_Do_List/to_do_manager.java
@@ -1,30 +1,32 @@
 package To_Do_List;
 
 public class to_do_manager {
-
+    //task table columns initialization
     String taskName;
     String taskDescription;
     String status;
     java.sql.Date timelimit;
     int project_id;
+    //constructor for all the columns
    public to_do_manager(int project_id, String taskName, String taskDescription, java.sql.Date timelimit){
         this.project_id = project_id;
         this.taskName = taskName;
         this.taskDescription = taskDescription;
         this.timelimit = timelimit;
-        
-        
     }
+    // constructor for project_id only
     public to_do_manager(int project_id){
         this.project_id = project_id;
     }
-  public to_do_manager (String status){
-    this.status = status;
-  }
-
-  public void setStatus(String status) {
-    this.status = status;
-}
+    //constructor for status only
+    public to_do_manager (String status){
+        this.status = status;
+    }
+    //setter for status
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    //getter for task name , description , timelimit, projectid
     public String getName(){
         return taskName;
     }
@@ -38,8 +40,9 @@ public class to_do_manager {
     public int getProjectId(){
         return project_id;
     }
+    //formatting the selected table contents
     @Override
-public String toString() {
-    return "Project id: " + this.project_id + ", task name: " + this.taskName + ", task description: "+ this.taskDescription+", time limit: " + this.timelimit;
-}
+    public String toString() {
+        return "Project id: " + this.project_id + ", task name: " + this.taskName + ", task description: "+ this.taskDescription+", time limit: " + this.timelimit;
+    }
 }
