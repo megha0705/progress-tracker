8384940e06926f28bdb10137c1688e757bd68d94
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2024-10-13 19:04:23 +0530

    comments added

--- To_Do_List/to_do_query.java
+++ To_Do_List/to_do_query.java
@@ -1,13 +1,14 @@
 package To_Do_List;
 
 public class to_do_query {
-   
+   //query for inserting the task data
    public static String getInsert(){
     String insert = "INSERT INTO task(taskName, taskDescription,timelimit,project_id, status) VALUES (?,?,?,?, 'pending')";
     return insert;
    }
+   //query for updating the status to complete and record the completion time
    public static String updateStatus(){
-      String update = "UPDATE task SET status = 'complete', completion_time = CURRENT_TIMESTAMP  WHERE project_id = ?";
+      String update = "UPDATE task SET status = 'complete', completion_time = CURRENT_TIMESTAMP  WHERE taskNo = ?";
       return update;
    }
 }
