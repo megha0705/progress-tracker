ac3d20255a3e3789fee86383b5bb9e02be1fb50c
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2024-10-14 22:05:19 +0530

    added comments

--- progress_bar/progress_DB_Connector.java
+++ progress_bar/progress_DB_Connector.java
@@ -1,16 +1,15 @@
 package progress_bar;
-import com.mysql.cj.jdbc.PreparedStatementWrapper;
-import com.mysql.cj.xdevapi.Result;
-import java.sql.ResultSet;
 
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import To_Do_List.to_do_manager;
 import database.DB_Connection;
 
 public class progress_DB_Connector {
-    public static void pj_progress(to_do_manager tdm){
-    
+    //function for calculating the project progress
+    public static double pj_progress(to_do_manager tdm){
+       double percentage = 0;
         try{
             Connection conn = DB_Connection.connect();
             String progressQuery = progress_query.progress();
@@ -20,12 +19,13 @@ public class progress_DB_Connector {
             while(rs.next()){
                 int total_task = rs.getInt(1);
                  int completed_task = rs.getInt(2);
-                double percentage = ((double)completed_task/total_task) * 100;
-                System.out.println("total progress of the project is = " + percentage + "%");
+                 percentage = ((double)completed_task/total_task) * 10;
+                //System.out.println("total progress of the project is = " + percentage + "%");
             }
 
         }catch(Exception e){
             e.printStackTrace();
         }
+        return percentage;
     }
 }
